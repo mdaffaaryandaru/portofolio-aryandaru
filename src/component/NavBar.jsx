253080f1c994b04851941895379e0ef0c11df8de
mdaffaaryandaru <daffaaryandaru100815@gmail.com>
2025-06-16 14:00:02 +0700

    change logo and fav description

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
-import logo from '../assets/img/logo.svg'
 import navIcon1 from '../assets/img/nav-icon1.svg'
 import navIcon2 from '../assets/img/nav-icon2.svg'
 import navIcon3 from '../assets/img/nav-icon3.svg'
 import { HashLink } from 'react-router-hash-link'
 import { BrowserRouter as Router } from 'react-router-dom'
+import '../style/Navbar.css'
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home')
@@ -34,7 +34,10 @@ export const NavBar = () => {
             <Navbar expand="md" className={scrolled ? 'scrolled' : ''}>
                 <Container>
                     <Navbar.Brand href="/">
-                        <img src={logo} alt="Logo" />
+                        <div className="logo-text">
+                            <span className="logo-first">Aryan</span>
+                            <span className="logo-last">daru</span>
+                        </div>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav">
                         <span className="navbar-toggler-icon"></span>
